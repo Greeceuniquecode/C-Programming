e18f93133e6e209935ae6fb09b6758b2a4feb86e
Greeceuniquecode <notgreece2@gmail.com>
2025-04-10 13:16:32 +0545

    Arrays

--- palindrome.c
+++ palindrome.c
@@ -2,23 +2,25 @@
 #include <conio.h>
 void main()
 {
-    int a, b, rev, rem;
+    int a,b,rev,rem;
     printf("enter number\n");
-    scanf("%d", &a);
-    b = a;
-    rev = 0;
-    while (b != 0)
+    scanf("%d",&a);
+    b=a;
+    rev=0;
+    while(b!=0)
     {
-        rem = b % 10;
-        rev = rev * 10 + rem;
-        b = b / 10;
+     rem=b%10;
+     rev=rev*10+rem;
+     b=b/10;   
     }
-    if (a == rev)
+    if (a==rev)
     {
-        printf("the number is palindrome\n");
+     printf("the number is palindrome");
     }
     else
     {
         printf("the number is not palindrome\n");
     }
+    
+    
 }
